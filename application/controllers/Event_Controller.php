350040ea76382d1ebe7c5d4efe6be6d26bf6e768
aflahvp <vpaflahafi@gmail.com>
2016-02-13 17:00:11 +0530

    updateddb

--- application/controllers/Event_Controller.php
+++ application/controllers/Event_Controller.php
@@ -10,10 +10,20 @@ class Event_Controller extends CI_Controller
 	public function __construct(argument)
 	{
 		parent::__construct();
-		$this->load->model('Member_Model');
+		$this->load->model('Event_Model');
 		$this->load->helper('form');
 		$this->load->helper('url');
 		$this->load->library('form_validation');
+	}
+
+	public function view_all()
+	{
+
+	}
+
+	public function add_events()
+	{
+		$this->form_validation->set_rules('eid', '', 'required');
 	}
 }
  ?>
