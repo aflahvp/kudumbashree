b76246d64422d3bb62c0de934521db89a38cd162
husnak <husnak.faisal@gmail.com>
2016-02-27 11:08:06 +0530

    modified add member

--- application/views/admin/add_member.php
+++ application/views/admin/add_member.php
@@ -7,75 +7,78 @@
 	<link rel="stylesheet" href="../../css/normalize.css">
 	<link rel="stylesheet" href="../../css/addmemberstyle.css">
 </head>
-<body>
+<body  bgcolor="#cccccc">
 <div class="member-box">
 	<h1>Add member</h1>
 	<?php echo form_open(base_url('Member_Controller/add_member'), ['id' => 'addmember', 'name' => 'addmember']);
 	echo validation_errors();
 ?>
-	<div class="group">
+	<div class="group width-100">
 	<label for="username">User Name</label>	
 	<input type="text" name="username" id="username" value="<?php echo set_value('username') ?>">
 	</div>
 	
-	<div class="group">
+	<div class="group width-100">
 	<label for="password">Password</label>	
 	<input type="password" name="password" id="password" value="<?php echo set_value('password') ?>">
 	</div>
 
-	<div class="group">
+	<div class="group width-100">
 	<label for="confirmpassword">Confirm Password</label>	
 	<input type="password" name="confirmpassword" id="confirmpassword" value="<?php echo set_value('confirmpassword') ?>">
 	</div>
 
-	<div class="group">
+	<div class="group width-100">
 	<label for="name">Name</label>	
 	<input type="text" name="name" id="name" value="<?php echo set_value('name') ?>">
 	</div>
 
-	<div class="group">
+	<div class="group width-100">
 	<label for="age">Age</label>	
 	<input type="text" name="age" id="age" value="<?php echo set_value('age') ?>">
 	</div>
 
-	<div class="group">
+	<div class="group width-100">
 	<label for="dob">Date of birth</label>	
 	<input type="text" name="dob" id="dob" value="<?php echo set_value('dob') ?>">
 	</div>
 
-	<div class="group">
+	<div class="group width-100">
 	<label for="address">Address</label>	
 	<textarea name="address" id="address" value=""><?php echo set_value('address') ?></textarea>
   	</div>
 
-  	<div class="group">
+  	<div class="group width-100">
   	<label for="place">Place</label>	
 	<input type="text" name="place" id="place" value="<?php echo set_value('place') ?>">
   	</div>
 
-  	<div class="group">
+  	<div class="group width-100">
 	  	<label for="spousesname">Spouses name</label>	
 		<input type="text" name="spousesname" id="spousesname" value="<?php echo set_value('spousesname') ?>">
   	</div>
 
-  	<div class="group">
+  	<div class="group width-100">
   	<label for="fathername">Father name</label>	
 	<input type="text" name="fathername" id="fathername" value="<?php echo set_value('fathername') ?>">
   	</div>
 
-  	<div class="group">
+  	<div class="group width-100">
   	<label for="mothername">Mother name</label>
 	<input type="text" name="mothername" id="mothername" value="<?php echo set_value('mothername') ?>">
   	</div>
 
-  	<div class="group">
+  	<div class="group width-100">
 	  	<span>Ration card type</span>
-	  	APL
-			<input type="radio" value="apl" name="ration" id="ration" <?php echo set_radio('ration', 'apl',true) ?>/>
-		BPL
-			<input type="radio" value="bpl" name="ration" id="ration" <?php echo set_radio('ration', 'apl') ?> >
+	  	<div class="radio">
+	  	<label for="apl">APL</label>
+			<input type="radio"  name="ration" id="apl" value="<?php echo set_radio('ration', 'apl' ); ?> " >
+		</div>	
+		<div class="radio">
+		<label for="apl">BPL</label>
+			<input type="radio"  name="ration" id="bpl" value="<?php echo set_radio('ration', 'bpl') ?> " >
   	</div>
-  	<div class="group">
+  	<div class="group width-100">
   	<label for="category">Category</label>
   	<select name="category" id="category">
   		<option value="" selected="" disabled="">select</option>
@@ -86,7 +89,7 @@
   	</select>
   	</div>
 
-	<div class="group">
+	<div class="group width-100">
 	<label for="bloodgroup">Blood group</label>
 	<select name="bloodgroup" id="bloodgroup">
   		<option value="" selected="" disabled="">select</option>
@@ -101,7 +104,7 @@
   	</select>
 	</div>
 
-	<div class="group">
+	<div class="group width-100">
 	<label for="eligibility">Eligibility</label>	
 	<select name="eligibility" id="eligibility">
   		<option value="" selected="" disabled="">select</option>
@@ -113,7 +116,7 @@
   	</select>
 	</div>
 
-	<div class="group">
+	<div class="group width-100">
 	<label for="religion">Religion</label>
 	<select name="religion" id="religion">
 		<option value="" selected="" disabled="">select</option>
@@ -124,11 +127,11 @@
   	</select>
 	</div>
 
-	<div class="group">
+	<div class="group width-100">
 	<label for="cast">Cast</label>
 	<input type="cast" name="cast" id="cast" value="<?php echo set_value('cast') ?>">
 	</div>
-	<div class="group">
+	<div class="group width-100">
 	<label for="district">District</label>	
 	<select name="district" id="district">
   		<option value="" selected="" disabled="">select</option>
@@ -149,42 +152,45 @@
   	</select>
 	</div>
 
-	<div class="group">
+	<div class="group width-100">
 	<label for="taluk">Taluk</label>	
 	<input type="text" name="taluk" id="taluk" value="<?php echo set_value('taluk') ?>">
 	</div>
 
-	<div class="group">
+	<div class="group width-100">
 	<label for="panchayath">Panchayath</label>	
 	<input type="text" name="panchayath" id="panchayath" value="<?php echo set_value('panchayath') ?>">
 	</div>
 
-	<div class="group">
+	<div class="group width-100">
 	<label for="mobileno">Mobile number</label>	
 	<input type="text" name="mobileno" id="mobileno" value="<?php echo set_value('mobileno') ?>">
 	</div>
 
-	<div class="group">
+	<div class="group width-100">
 	<label for="accountno">Account number</label>	
 	<input type="text" name="accountno" id="accountno" value="<?php echo set_value('accountno') ?>">
 	</div>
 
 
-	<div class="group">
+	<div class="group width-100">
 	<label for="adharno">Adhar number</label>	
 	<input type="text" name="adharno" id="adharno" value="<?php echo set_value('adharno') ?>">
 	</div>
 
-	<div class="group">
+	<div class="group width-100">
 	<label for="email">E-mail id</label>	
 	<input type="text" name="email" id="email" value="<?php echo set_value('email') ?>">
 	</div>	
 	
-	<div class="group txt-center">
+	<div class="group width-100 txt-center">
 		<input type="submit" name="submit" value="submit" id="submit" class="btn-submit">
 		<input type="reset" name="rest" value="reset" id="reset" class="btn-submit">
 		<input type="button" name="cancel" value="cancel" id="cancel" class="btn-submit">
 	</div>
+	<?php if (isset($message)): ?>
+		<?php echo $message ?>
+<?php endif ?>
 </form>
 </body>
 </html>
