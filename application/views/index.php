d2ae28117899d300d40f666fc26d00aa1ca31aec
noushid <pnoushid@gmail.com>
2016-04-10 10:10:08 +0530

    importing temlate

---
+++ application/views/index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>E-kudumbasree</title>
+<link href="<?php echo base_url('css/templatemo_style.css') ?>" rel="stylesheet" type="text/css" />
+<script language="javascript" type="text/javascript">
+function clearText(field)
+{
+    if (field.defaultValue == field.value) field.value = '';
+    else if (field.value == '') field.value = field.defaultValue;
+}
+</script>
+</head>
+<body>
+<div id="templatemo_header_wrapper">
+<!--  Free Web Templates by TemplateMo.com  -->
+  <div id="templatemo_header">
+    
+   	<div id="site_logo"></div>
+        
+		<div id="templatemo_menu">
+      		<div id="templatemo_menu_left"></div>
+            <ul>
+                  <li><a href="#" class="current">Home</a></li>
+                  <!-- <li><a href="">Services</a></li> -->
+                  <!-- <li><a href="">Gallery</a></li> -->
+                  <li><a href="<?php echo base_url('admin-login') ?>">admin</a></li>
+                  <li><a href="<?php echo base_url('member/login') ?>" class="last">member login</a></li>
+            </ul>    	
+		</div> <!-- end of menu -->
+    
+    </div>  <!-- end of header -->
+</div> <!-- end of header wrapper -->
+
+<div id="templatemo_banner_wrapper">
+	<div id="templatemo_banner">
+    
+    	<div id="templatemo_banner_image">
+        	<div id="templatemo_banner_image_wrapper">
+            	<img src="images/templatemo_image_01.jpg" alt="image 1" />
+            </div>
+        </div>
+        
+        <div id="templatemo_banner_content">
+        	<div class="header_01">KUDUMBASREE : A TORCH OF PROSPERITY FOR THE POOR </div>
+            <p>Kudumbasree/CDS units in Kerala play a crucial role in the implementation of the scheme</p>
+            <div class="button_01"><a href="#">Read more</a></div>
+        </div>	
+    
+    	<div class="cleaner"></div>
+    </div> <!-- end of banner -->
+</div> <!-- end of banner wrapper -->
+
+<div id="templatemo_content_wrapper">
+	<div id="templatemo_content">
+    
+    	<div id="column_w530">
+        	
+            <div class="header_02">Welcome to E-KUDUMBASREE!</div>
+            
+            <p class="em_text">There are two distinguishing characteristics to Kudumbashree which set it apart from the usual SHG model of empowerment. These are.</p>
+            
+            <p>The universality of reach – from its very inception Kudumbashree has attempted to bring every poor woman in the state within its fold, as a consequence of which today Kudumbashree is present in every village panchayat and municipality,  and in nearly every ward, colony and hamlet.</p>
+            <div class="margin_bottom_20"></div>
+            
+            <ul class="content_list_01">
+                <li>.The scope of community interface in local governance – the functioning of Kudumbashree is tied up to the development
+initiatives of the local government be it for social infrastructure.</li>
+                <li>From food security to health insurance, from housing to enterprise development, from the national wage
+employment programme to the jagratha samiti, every development experience depends on Kudumbashree to provide the
+community interface.</li>
+            </ul>
+            
+            <div class="margin_bottom_20"></div>           
+            
+            <div class="content_section_01">
+            	 "To eradicate absolute poverty in ten years through concerted community action under the leadership of local governments, by facilitating organization of the poor for combining self-help with demand-led convergence of available services and resources to tackle the multiple dimensions and manifestations of poverty, holistically."
+            </div>
+            
+            <div class="cleaner"></div>
+        </div>
+        
+        <div id="column_w300">
+        
+        	<div class="header_03">Latest News</div>
+            
+            <div class="column_w300_section_01">
+            	<div class="news_image_wrapper">
+                	<img src="<?php echo base_url('images/templatemo_image_02.jpg') ?>" alt="image" />
+                </div>
+                
+                <div class="news_content">
+                	<div class="news_date">JAN 29, 2015</div>
+                    <div class="header_04"><a href="#">initial working started</a></div>
+                    <p>The initial stage started on january 29th .</p>
+				</div>
+                                
+                <div class="cleaner"></div>
+            </div>
+            
+            <div class="column_w300_section_01 even_color">
+            	<div class="news_image_wrapper">
+                	<img src="<?php echo base_url('images/templatemo_image_03.jpg') ?>" alt="image" />
+                </div>
+                
+                <div class="news_content">
+                	<div class="news_date">FEB 28, 2016</div>
+                    <div class="header_04"><a href="#">Implimenting the concept.</a></div>
+                    <p>Our concept was implimented on feb 28th .</p>
+				</div>
+                                
+                <div class="cleaner"></div>
+            </div>
+            
+            <div class="column_w300_section_01">
+            	<div class="news_image_wrapper">
+                	<img src="<?php echo base_url('images/templatemo_image_04.jpg') ?>" alt="image" />
+                </div>
+                
+                <div class="news_content">
+                	<div class="news_date">MAR 18, 2016</div>
+                    <div class="header_04"><a href="#">Lounge the website</a></div>
+                    <p>website implimented on march 5th "E-KUDUMBASREE".</p>
+				</div>
+                                
+                <div class="cleaner"></div>
+            </div>
+            
+            <div class="cleaner"></div>
+        </div>
+    
+    	<div class="cleaner"></div>
+    </div> <!-- end of content wrapper -->
+</div> <!-- end of content wrapper -->
+
+<div id="templatemo_footer_wrapper">
+
+	<div id="templatemo_footer">
+    	
+        <!-- <div class="section_w180">
+        	<div class="header_05">Services</div>
+            <div class="section_w180_content"> 
+            	<ul class="footer_menu_list">
+                    <li><a href="#">Lorem ipsum dolor</a></li>
+                    <li><a href="#">Cum sociis</a></li>
+                    <li><a href="#">Donec quam</a></li>
+                    <li><a href="#">Nulla consequat</a></li>
+                    <li><a href="#">In enim justo</a></li>              
+                </ul>
+			</div>
+        </div>
+        
+        <div class="section_w180">
+        	<div class="header_05">About</div>
+            <div class="section_w180_content">
+                <ul class="footer_menu_list">
+                    <li><a href="#">Nullam quis</a></li>
+                    <li><a href="#">Sed consequat</a></li>
+                    <li><a href="#">Cras dapibus</a></li> 
+                    <li><a href="#">Lorem ipsum dolor</a></li>
+                    <li><a href="#">Cum sociis</a></li>                
+                </ul>
+            </div>
+        </div>
+                
+        <div class="section_w180">
+        	<div class="header_05">Cool links</div>
+            <div class="section_w180_content"> 
+                <ul class="footer_menu_list">
+                    <li><a href="#">Aenean vulputate</a></li>
+                    <li><a href="#">Etiam ultricies</a></li>
+                    <li><a href="#">Nullam quis</a></li>
+                    <li><a href="#">Sed consequat</a></li>
+                    <li><a href="#">Cras dapibus</a></li>             
+                </ul>
+			</div>
+        </div>
+        
+        <div class="section_w180">
+	        <div class="header_05">Contact</div>
+            
+            <div class="section_w180_content">
+            
+                <ul class="footer_menu_list">
+                    <li><a href="#">Donec quam</a></li>
+                    <li><a href="#">Nulla consequat</a></li>
+                    <li><a href="#">In enim justo</a></li>    
+                    <li><a href="#">Aenean vulputate</a></li>
+                    <li><a href="#">Etiam ultricies</a></li>           
+                </ul>
+			</div>
+        </div> -->
+        
+       <!--  <div class="section_w180">
+                   <div class="header_05">Partners</div>  
+           <div class="section_w180_content">      
+               <ul class="footer_menu_list">
+                   <li><a href="http://www.templatemo.com" target="_parent">CSS Templates</a></li>
+                   <li><a href="http://www.flashmo.com" target="_parent">Flash Websites</a></li>
+                   <li><a href="http://www.templatemo.com/page/2" target="_parent">Website Templates</a></li>
+                   <li><a href="http://www.webdesignmo.com" target="_parent">Web Design Tips</a></li>
+                   <li><a href="http://www.photovaco.com" target="_blank">Free Stock Photos</a></li>
+               </ul>
+                   </div>
+       </div> -->
+        
+        <div class="margin_bottom_20"></div>
+        Copyright © 2048 <a href="#">jamia collage</a> | Website develeped by by Husna And team <div class="cleaner"></div>
+    </div> <!-- end of footer -->
+    <!--  Free CSS Templates by TemplateMo.com  -->
+</div> <!-- end of footer -->
+</html>
