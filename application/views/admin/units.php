221aac87d8c6ce26603953b83da9f2dd922c9f2f
husnak <husnak.faisal@gmail.com>
2016-04-12 18:30:15 +0530

    yyyyyy

--- application/views/admin/units.php
+++ application/views/admin/units.php
@@ -6,7 +6,7 @@
   <meta name="description" content="Psybo technologies is a small web design &amp; development agency based in Manjeri, Malappuram, INDIA. We've made a reputation for building websites that look great and are easy-to-use.">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" href="<?php echo base_url('img/ico.png');?>" type="image/png" sizes="47x54">
-  <title><?php echo ucfirst($currentPage); ?> - E - KUDUMBA`SHREE</title>
+  <title>- E - KUDUMBA`SHREE</title>
   <link rel="stylesheet" href="<?php echo base_url('css/styleapp.css');?>">
   <script type="text/javascript" src="<?php echo base_url('js/appjs.js');?>"></script>
   <style>
@@ -14,9 +14,22 @@
           width: 10px;
           height: 10px;
       }
+
+      #container
+      {
+        width:50%;
+      height:100%;
+      margin:5% 0% 0% 25%;
+      background:pink;
+      background-position:center;
+      border-radius:9px;
+      /* box-shadow:rgb(73, 73, 73) 0px 10px 20px; */
+      box-shadow:0px 20px 20px black;
+      }
   </style>
 </head>
 <body>
+
   <div class="page-wrapper">
     <div class="left-wrapper">
       <?php echo dashboard_menu('units');?>
@@ -41,38 +54,38 @@
 <?php echo form_open(base_url('dashboard/units/add/submit'), ['id' => 'addunits', 'name' => 'addunits']);
 echo validation_errors();
 ?>
- 
+  <div id ="container" align="center"><br/><br/>
  	<div class="group">
-		<label for="name">Name</label>
+		<label for="name">Name : </label>
 		<input type="text" name="name" id="name" value="">
  	</div>
   	<div class="group">
-       <label for="ward">Ward</label>
+       <label for="ward">Ward : </label>
   	   <input type="text" name="ward" id="ward" value="">
 	</div>
     <div class="group">
-		<lable for="address">Adress</lable>
+		<lable for="address">Adress : </lable>
 		<textarea name="address" id="address" value=""></textarea>
   	</div>
   	<div class="group">
-  		<label for="place">Place</label>	
+  		<label for="place">Place : </label>	
 	  	<input type="text" name="place" id="place" value="">
   	</div>	
 
    
    <div class="group">
-	 <label for="username">User Name</label>
+	 <label for="username">User Name : </label>
 	 <input type="text" name="username" id="username" placeholder="User Name">
    </div>
     <div class="group">
-	 <label for="password">Password</label>
+	 <label for="password">Password : </label>
 
 	 <input type="password" name="password" id="password" placeholder=" PassWord">
 
 		</div>
 
      <div class="group">
-   <label for="usertype">User Type</label> 
+   <label for="usertype">User Type : </label> 
       <select class="width-60" name="usertype" id="usertype">
           <option value="" selected="" disabled="">select</option>
           <option value="secretary"  <?php echo  set_select('usertype', 'secretary' ); ?>>----Secretary----</option>
@@ -86,6 +99,8 @@ echo validation_errors();
 
 		<input type="reset" name="cancel" value="Cancel" class="btn-submit">
 	</div>
+  <br/><br/>
+  </div>
 </form>
 </div>
 </div>
